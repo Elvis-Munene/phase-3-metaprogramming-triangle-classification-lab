7f203e6cd86450312a57ffaad7b9565ecec65f5e
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-08-21 10:55:23 +0300

    commit tree b3eabceb7849b9541bf25ac755c767058e14db7c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
-  # write code here
+ 
+  attr_reader :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  #Triangle has three sides.(Dimensions x,y,z)
+  #Type of triangle is determined by the size of the lengths.
+  #  Equilateral x =y =z
+  # Isosceles x=y or x=z or y=z
+  # scalene x ! y ! z 
+
+  def kind
+    valid_triangle
+    if x==y && y==z
+      :equilateral
+    elsif x==y || x==z || y==z
+      :isosceles
+    else 
+      :scalene   
+  end
 end
+
+
+def valid_triangle
+    valid_tria = [(x+y > z), (x+ z > y), (y+z >x)]
+    [x,y,z].each do |side|
+      valid_tria << false if side <= 0
+      raise TriangleError if valid_tria.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+  end
+
+
+
